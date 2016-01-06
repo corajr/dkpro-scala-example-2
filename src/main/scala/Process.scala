312651c01177509b73a43e5526d664511dfbfcbd
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2016-01-06 15:15:24 -0500

    wrap JCasUtil.select

--- src/main/scala/Process.scala
+++ src/main/scala/Process.scala
@@ -1,9 +1,10 @@
 import org.apache.uima.analysis_engine.AnalysisEngineDescription
 import org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription
 import org.apache.uima.fit.pipeline.SimplePipeline.iteratePipeline
+import org.apache.uima.fit.util.JCasUtil
 import org.apache.uima.jcas.JCas
+import org.apache.uima.jcas.cas.TOP
 import de.tudarmstadt.ukp.dkpro.core.clearnlp.{ClearNlpSegmenter, ClearNlpLemmatizer, ClearNlpPosTagger}
-
 
 case class Process(engines: Seq[AnalysisEngineDescription]) {
   import scala.collection.JavaConversions._
@@ -23,4 +24,11 @@ object Process {
     )
 
   def apply(engines: AnalysisEngineDescription*)(implicit d: DummyImplicit) = new Process(Seq(engines: _*))
+  
+  implicit class EnrichedJCas(jcas: JCas) {
+    import scala.collection.JavaConversions._
+
+    def select[T <: TOP](clazz: Class[T]): Iterator[T] = JCasUtil.select(jcas, clazz).iterator()
+    def selectSingle[T <: TOP](clazz: Class[T]): T = JCasUtil.selectSingle(jcas, clazz)
+  }
 }
--- src/test/scala/ProcessSpec.scala
+++ src/test/scala/ProcessSpec.scala
@@ -1,5 +1,4 @@
 import org.scalatest._
-import org.apache.uima.fit.util.JCasUtil
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.`type`.DocumentMetaData
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.`type`.Lemma
 
@@ -8,28 +7,28 @@ class ProcessSpec extends FunSpec with Matchers {
     getClass().getClassLoader().getResource("inaugural/").getPath()
   
   describe("Process") {
+    import Process._
+
     describe("lemmatize") {
       describe("when passed a reader") {
         it("should lemmatize a corpus") {
           val corpus = Corpus.fromDir(corpusDir)
           val lemmaMap = (for {
             jcas <- Process.lemmatize(corpus)
-            metadata = JCasUtil.selectSingle(jcas, classOf[DocumentMetaData])
+            metadata = jcas.selectSingle(classOf[DocumentMetaData])
             title = metadata.getDocumentTitle()
-            lemmas = JCasUtil.select(jcas, classOf[Lemma])
-          } yield title -> lemmas.size()).toMap
+            lemmas = jcas.select(classOf[Lemma])
+          } yield title -> lemmas.size).toMap
           lemmaMap should have size 56
           all (lemmaMap.values) should be > 0
         }
 
-        it("should return the expected lemmas from part of the first document") {
-          import scala.collection.JavaConversions._
-          
+        it("should return the expected lemmas from part of the first document") {          
           val corpus = Corpus.fromDir(corpusDir)
           val jcasIterator = Process.lemmatize(corpus)
           val jcas = jcasIterator.next()
-          val lemmas = JCasUtil.select(jcas, classOf[Lemma]).take(5).map(_.getValue)
-          lemmas shouldBe List("fellow", "-", "citizen", "of", "the")
+          val lemmas = jcas.select(classOf[Lemma]).take(5).map(_.getValue)
+          lemmas.toSeq shouldBe Seq("fellow", "-", "citizen", "of", "the")
         }
       }
     }
