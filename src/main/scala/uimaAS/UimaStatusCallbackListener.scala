9c32a4cde09e75ee233d2483ec2ac970c26d46d7
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2016-01-12 12:12:15 -0500

    more test

--- src/main/scala/uimaAS/UimaStatusCallbackListener.scala
+++ src/main/scala/uimaAS/UimaStatusCallbackListener.scala
@@ -18,76 +18,80 @@ class UimaStatusCallbackListener(
   var logCas: Boolean = true
   val casMap: concurrent.Map[String, Long] = concurrent.TrieMap()
 
-  def stopOnErr(status: EntityProcessStatus, msg: String, ignoreErrors: Boolean = false): Unit = {
+  def stopOnErr(status: EntityProcessStatus, msg: String, ignoreErrors: Boolean = false)(block: => Unit): Unit = {
     if (status != null && status.isException()) {
       System.err.println(msg)
       val exceptions = status.getExceptions()
       for (e <- exceptions) {
         e.printStackTrace()
       }
+
       if (!ignoreErrors) {
         System.err.println("Terminating Client...")
         engine.stop()
-      }
-    }
-  }
-
-  override def initializationComplete(status: EntityProcessStatus): Unit = {
-    stopOnErr(status, "Error on getMeta call to remote service:")
-  }
-
-  override def collectionProcessComplete(status: EntityProcessStatus): Unit = {
-    stopOnErr(status, "Error on collection process complete call to remote service:")
-    System.out.print("Completed " + entityCount + " documents")
-    if (size > 0) {
-      System.out.print("; " + size + " characters");
-    }
-    System.out.println();
-    val elapsedTime = System.nanoTime() / 1000000 - startTime
-    System.out.println("Time Elapsed : " + elapsedTime + " ms ");
-
-    val perfReport = engine.getPerformanceReport()
-    if (perfReport != null) {
-      System.out.println("\n\n ------------------ PERFORMANCE REPORT ------------------\n");
-      System.out.println(perfReport);
-    }
-  }
-
-  override def entityProcessComplete(cas: CAS, status: EntityProcessStatus): Unit = {
-    stopOnErr(status, "Error on process CAS call to remote service:", true)
-    if (logCas) {
-      var ip: Option[String] = None
-      val events = status.getProcessTrace.getEventsByComponentName("UimaEE", false)
-      for (event <- events) {
-        if (event.getDescription().equals("Service IP")) {
-          ip = Some(event.getResultMessage())
-        }
-      }
-
-      val casId = status.asInstanceOf[UimaASProcessStatus].getCasReferenceId
-      if (casId != null) {
-        val current = System.nanoTime() / 1000000 - startTime
-        casMap.get(casId).foreach { start =>
-          System.out.println(ip + "\t" + start + "\t" + (current - start))
-        }
       } else {
-        System.out.print(".");
-        if (0 == (entityCount + 1) % 50) {
-          System.out.print((entityCount + 1) + " processed\n");
-        }
+        block
       }
-
-      outputDir.foreach { dir =>
-        Util.serializeCas(cas, dir, entityCount)
-      }
-
-      entityCount = entityCount + 1
-      val docText = cas.getDocumentText
-      if (docText != null) {
-        size += docText.length
-      }
+    } else {
+      block
     }
   }
+
+  override def initializationComplete(status: EntityProcessStatus): Unit =
+    stopOnErr(status, "Error on getMeta call to remote service:") {}
+
+  override def collectionProcessComplete(status: EntityProcessStatus): Unit =
+    stopOnErr(status, "Error on collection process complete call to remote service:") {
+      System.out.print("Completed " + entityCount + " documents")
+      if (size > 0) {
+        System.out.print("; " + size + " characters");
+      }
+      System.out.println();
+      val elapsedTime = System.nanoTime() / 1000000 - startTime
+      System.out.println("Time Elapsed : " + elapsedTime + " ms ");
+
+      val perfReport = engine.getPerformanceReport()
+      if (perfReport != null) {
+        System.out.println("\n\n ------------------ PERFORMANCE REPORT ------------------\n");
+        System.out.println(perfReport);
+      }
+    }
+
+  override def entityProcessComplete(cas: CAS, status: EntityProcessStatus): Unit =
+    stopOnErr(status, "Error on process CAS call to remote service:", true) {
+      if (logCas) {
+        var ip: Option[String] = None
+        val events = status.getProcessTrace.getEventsByComponentName("UimaEE", false)
+        for (event <- events) {
+          if (event.getDescription().equals("Service IP")) {
+            ip = Some(event.getResultMessage())
+          }
+        }
+
+        val casId = status.asInstanceOf[UimaASProcessStatus].getCasReferenceId
+        if (casId != null) {
+          val current = System.nanoTime() / 1000000 - startTime
+          casMap.get(casId).foreach { start =>
+            System.out.println(ip + "\t" + start + "\t" + (current - start))
+          }
+        } else {
+          System.out.print(".");
+          if (0 == (entityCount + 1) % 50) {
+            System.out.print((entityCount + 1) + " processed\n");
+          }
+        }
+
+        outputDir.foreach { dir =>
+          Util.serializeCas(cas, dir, entityCount)
+        }
+
+        entityCount = entityCount + 1
+        val docText = cas.getDocumentText
+        if (docText != null) {
+          size += docText.length
+        }
+      }
+    }
 
   override def onBeforeMessageSend(status: UimaASProcessStatus): Unit = {
     val current = System.nanoTime() / 1000000 - startTime
--- src/test/scala/uimaAS/UimaStatusCallbackListenerSpec.scala
+++ src/test/scala/uimaAS/UimaStatusCallbackListenerSpec.scala
@@ -7,13 +7,30 @@ import org.apache.uima.collection.EntityProcessStatus
 import org.jmock.api.Action
 import org.jmock.lib.action.ReturnValueAction
 import org.jmock.lib.action.ReturnIteratorAction
+import org.apache.uima.aae.client.UimaAsBaseCallbackListener
 
 class UimaStatusCallbackListenerSpec extends FunSpec {
   def returnValue[T](result: T): Action =
     new ReturnValueAction(result)
 
   def emptyList[T]: java.util.List[T] = new java.util.LinkedList[T]()
-  
+
+  def expectStopOnErr(cycle: JMockCycle)
+                     (block: (UimaAsBaseCallbackListener, EntityProcessStatus) => Unit): Unit = {
+    import cycle._
+
+    val engineMock = mock[UimaAsynchronousEngine]
+    val statusMock = mock[EntityProcessStatus]
+
+    expecting { e => import e._
+      oneOf (statusMock).isException(); will(returnValue(true))
+      oneOf (statusMock).getExceptions(); will(returnValue(emptyList[Exception]))
+      oneOf (engineMock).stop()
+    }
+    val statusListener = new UimaStatusCallbackListener(engineMock)
+    whenExecuting(block(statusListener, statusMock))
+  }
+
   describe("UimaStatusCallbackListener") {
     describe("initializationComplete") {
       describe("when called normally") {
@@ -24,10 +41,10 @@ class UimaStatusCallbackListenerSpec extends FunSpec {
           val engineMock = mock[UimaAsynchronousEngine]
           expecting { e => import e._
           }
-          
+
           val statusListener = new UimaStatusCallbackListener(engineMock)
           val status = null
-          
+
           whenExecuting {
             statusListener.initializationComplete(status)
           }
@@ -36,21 +53,38 @@ class UimaStatusCallbackListenerSpec extends FunSpec {
       describe("when called with an error") {
         it("should call stop on the engine") {
           val cycle = new JMockCycle
+          expectStopOnErr(cycle) { (statusListener, status) =>
+            statusListener.initializationComplete(status)
+          }
+        }
+      }
+    }
+
+    describe("collectionProcessComplete") {
+      describe("when called normally") {
+        it("should do nothing") {
+          val cycle = new JMockCycle
           import cycle._
 
           val engineMock = mock[UimaAsynchronousEngine]
-          val statusMock = mock[EntityProcessStatus]
           expecting { e => import e._
-            oneOf (statusMock).isException(); will(returnValue(true))
-            oneOf (statusMock).getExceptions(); will(returnValue(emptyList[Exception]))
-            oneOf (engineMock).stop()
+            oneOf (engineMock).getPerformanceReport(); will(returnValue(null))
           }
-          
+
           val statusListener = new UimaStatusCallbackListener(engineMock)
-          
+          val status = null
+
           whenExecuting {
-            statusListener.initializationComplete(statusMock)
-          }        
+            statusListener.collectionProcessComplete(status)
+          }
+        }
+      }
+      describe("when called with an error") {
+        it("should call stop on the engine") {
+          val cycle = new JMockCycle
+          expectStopOnErr(cycle) { (statusListener, status) =>
+            statusListener.collectionProcessComplete(status)
+          }
         }
       }
     }
